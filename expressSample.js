ec8eb6db737d7cdd4fd98985f9527ff0b87c34cd
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-02 10:49:54 -0600

    sample change

--- expressSample.js
+++ expressSample.js
@@ -2,7 +2,13 @@ let express = require('express');
 
 let app = express();
 
+//app.set is used to set the template engine
 app.set('view engine', 'ejs');
+
+//app.use is used for middleware. If you specify a path, you're telling it to only run on that route. If you don't specify a route, it's going to run on every single request.
+//The next parameter tells express to run the next piece of middleware, after the current middleware target has been executed.
+app.use('/assets', express.static('assets'))
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
