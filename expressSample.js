3eb6f6dc459b384c7502199408b8be54c919fc56
Jahaziel <jahaziel.vaz@gmail.com>
2019-10-02 11:38:54 -0600

    Stylesheets are now re-directed to assets, and the requests have been properly made

--- expressSample.js
+++ expressSample.js
@@ -3,15 +3,20 @@ let express = require('express');
 let app = express();
 
 //app.set is used to set the template engine
-app.set('view engine', 'ejs');
+
 
 //app.use is used for middleware. If you specify a path, you're telling it to only run on that route. If you don't specify a route, it's going to run on every single request.
 //The next parameter tells express to run the next piece of middleware, after the current middleware target has been executed.
-app.use('/assets', express.static('assets'))
+app.set('view engine', 'ejs');
+app.use('/assets', express.static('./views/assets'));
 
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// })
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.render('index', {stuff: 'Welcome'})
 })
 
 app.get('/sample/:profile', (req, res) => {
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 let http = require('http'),
 fs = require('fs')
 
-let server = http.createServer((req, res) => {
-  if(req.url === '/home' || req.url === '/'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/index.html').pipe(res);
-  } else if(req.url === '/contact'){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.createReadStream(__dirname + '/contact.html').pipe(res);
-  } else {
-    res.writeHead(404, {'Content-Type': 'text-plain'});
-    fs.createReadStream(__dirname + '/404.html').pipe(res);
-  }
+// let server = http.createServer((req, res) => {
+//   if(req.url === '/home' || req.url === '/'){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     fs.createReadStream(__dirname + '/index.html').pipe(res);
+//   } else if(req.url === '/contact'){
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     fs.createReadStream(__dirname + '/contact.html').pipe(res);
+//   } else {
+//     res.writeHead(404, {'Content-Type': 'text-plain'});
+//     fs.createReadStream(__dirname + '/404.html').pipe(res);
+//   }
 
   // let myObj = {
   //   name: 'Jazzy',
@@ -21,5 +21,5 @@ let server = http.createServer((req, res) => {
   // res.end(JSON.stringify(myObj));
 })
 
-console.log('Server running on port 3000');
-server.listen(3000);
+// console.log('Server running on port 3000');
+// server.listen(3000);
